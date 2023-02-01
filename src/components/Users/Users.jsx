c3737b45d05a43bfae1595bcecce50dca37b5de9
Тарас Іванович <vip.kristals@gmail.com>
2023-02-01 09:42:10 +0200

    44 - UserApi - get

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,21 @@
 import React from "react";
 import styles from "./users.module.css"
+import axios from "axios";
+import userPhoto from '../../assets/images/user.jpg'
 
 let Users = (props) =>{
     if(props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: "https://images.pexels.com/photos/3378993/pexels-photo-3378993.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                fullName: 'Aliqa Macale',
-                email: "support@gmail.com",
-                followed: true,
-                location: {city: "Ivano-Frankivsk", country: "Ukraine"}
-            },
-            {
-                id: 2,
-                photoUrl: "https://images.pexels.com/photos/3378993/pexels-photo-3378993.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                fullName: 'Taras Macale',
-                email: "support@gmail.com",
-                followed: false,
-                location: {city: "Chernivtsi", country: "Ukraine"}
-            },
-            {
-                id: 3,
-                photoUrl: "https://images.pexels.com/photos/3378993/pexels-photo-3378993.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                fullName: 'Vasyl Macale',
-                email: "support@gmail.com",
-                followed: true,
-                location: {city: "Ivano-Frankivsk", country: "Ukraine"}
-            },
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response =>{
+            props.setUsers(response.data.items)
+        })
     }
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.photoUser}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.photoUser}/>
                     </div>
                      <div>
                     {u.followed
@@ -44,8 +24,8 @@ let Users = (props) =>{
                     </div>
                 </span>
                 <span>
-                    <div>{u.fullName}</div>
-                    <div>{u.location.city},{u.location.country}</div>
+                    <div>{u.name}</div>
+                    <div>{"u.location.city"},{"u.location.country"}</div>
                 </span>
             </div>)
         }
