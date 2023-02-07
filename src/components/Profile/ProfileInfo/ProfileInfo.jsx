53a41a767c663959c14cb49f88156243cf73bbc9
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-07 14:10:08 +0200

    53 - withRouter, props.match.params

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -14,7 +14,7 @@ const ProfileInfo = (props) => {
             </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
-                ava + decription</div>
+                {props.profile.aboutMe}</div>
         </div>
     );
 };
