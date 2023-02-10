89b66220d2ee187e717ce063c082ca7b43e681ec
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-10 12:46:39 +0200

    56/1 - DAL, axios.create, promise

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials:true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers:{
+        "API-KEY":"c7498302-c136-4b6a-9b8f-7de789b16fc9s"
+    }
+})
+
+export const userAPI = {
+   getUsers(currentPage = 1, pageSize = 10){
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            });
+    }
+}
+
+
