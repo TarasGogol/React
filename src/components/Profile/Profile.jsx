f3ae6502560d24dafa464c5842884d86665daf63
TarasGogol <72978940+TarasGogol@users.noreply.github.com>
2023-02-14 10:51:31 +0200

    59 - redirect - navigate

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,8 +3,8 @@ import s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-const Profile = (props) => {
 
+const Profile = (props) => {
     return (
         <div className={s.content}>
             <ProfileInfo profile={props.profile}/>
